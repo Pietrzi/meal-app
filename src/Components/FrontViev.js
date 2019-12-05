b098efdf4b677a7e096c362bbe00a90932283022
Pietrzi <pietrzi@op.pl>
2019-12-05 20:25:18 +0100

    first commit

---
+++ src/Components/FrontViev.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FrontViev extends Component {
+    render() {
+        return (
+            <div>
+                jol
+            </div>
+        )
+    }
+}
