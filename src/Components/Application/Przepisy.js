83e038d3c91f153ee08bfa7f1e13d6a618d1fba2
Pietrzi <pietrzi@op.pl>
2019-12-11 21:09:30 +0100

    pulpit and przepisy components and style done

--- src/Components/Application/Przepisy.js
+++ src/Components/Application/Przepisy.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from 'react';
+// import { fetchRecipes } from '../../redux/recipes/actions';
+// import { connect } from 'react-redux';
 
-export default function Przepisy() {
-    return (
-        <div>
-            przepisy
-        </div>
-    )
+class Przepisy extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes: [
+                {id: 1, name: "Zupa Pomidorowa", description: "Pyszna zupa pomidorowa z całych pomidorów.", steps: ["Wlej wodę do garnka", "Wrzuć pomidory i zagotuj"], ingredients: ["Pomidory", "Woda"]},
+                {id: 2, name: "Zupa Jarzynowa", description: "Pyszna zupa jarzynowa z całych warzyw.", steps: ["Wlej wodę do garnka", "Wrzuć warzywa i zagotuj"], ingredients: ["Warzywa", "Woda"]},
+                {id: 3, name: "Zupa Szczawiowa", description: "Pyszna zupa szczawiowa z całych szczawiów.", steps: ["Wlej wodę do garnka", "Wrzuć szczaw i zagotuj"], ingredients: ["Szczaw", "Woda"]},
+                {id: 4, name: "Zupa Ogórkowa", description: "Pyszna zupa ogórkowa z całych ogórków.", steps: ["Wlej wodę do garnka", "Wrzuć ogórki i zagotuj"], ingredients: ["Ogórki", "Woda"]},
+                {id: 5, name: "Zupa Cebulowa", description: "Pyszna zupa cebulowa z całych cebul.", steps: ["Wlej wodę do garnka", "Wrzuć cebulę i zagotuj"], ingredients: ["Cebula", "Woda"]},
+                {id: 6, name: "Zupa Truskawkowa", description: "Pyszna zupa truskawkowa z całych truskawek.", steps: ["Wlej wodę do garnka", "Wrzuć truskawki i zagotuj"], ingredients: ["Truskawki", "Woda"]},
+            ]
+        }
+    }
+
+    // componentDidMount() {
+    //     this.props.fetchRecipes();
+    // }
+
+    render() {
+
+        const recipesList = this.state.recipes.length ? (
+            this.state.recipes.map(recip => {
+                return (
+                    <tr key={recip.id}>
+                        <td>{recip.id}</td>
+                        <td>{recip.name}</td>
+                        <td>{recip.description}</td>
+                        <td className="icons">
+                            <i className="edit" class="fa fa-edit"></i>
+                            <i className="delete" class="fa fa-trash"></i>
+                        </td>
+                    </tr>
+                )
+            })
+        ) : (
+            null
+        );
+
+        return (
+            <div className="recipes__wrapper">
+                <div className="recipes__header">
+                    <h2>Lista Przepisów</h2>
+                    <i class="fa fa-plus-square"></i>
+                </div>
+                <div className="table__container">
+                    <table>
+                        <thead>
+                          <tr className="table__header">
+                            <th className="id">id</th>
+                            <th className="name">nazwa</th>
+                            <th className="description">opis</th>
+                            <th className="actions">akcje</th>
+                          </tr>
+                          </thead>
+                        <tbody>
+                            { recipesList }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+   
 }
+
+
+// const mapStateToProps = state => ({
+//     recipes: state.recipes.recipes
+// })
+
+// export default connect(mapStateToProps, { fetchRecipes })(Przepisy);
+
+export default Przepisy;
--- src/Components/Application/Pulpit.js
+++ src/Components/Application/Pulpit.js
@@ -1,9 +1,147 @@
-import React from 'react'
+import React from 'react';
+import "font-awesome/css/font-awesome.min.css";
 
-export default function Pulpit() {
+const Pulpit = () => {
     return (
-        <div>
-            pulpit
+        <div className="pulpit">
+            <div className="dashboard">
+                <div className="dashboard__widgets">
+                    <div className="add_widget__container">
+                        <div className="add_widget__box">
+                            <div className="add__widget recipe">
+                                <i class="fa fa-plus"></i>
+                            </div>
+                            <p>dodaj przepis</p>
+                        </div>
+                        <div className="add_widget__box">
+                            <div className="add__widget plan">
+                                <i class="fa fa-plus"></i>
+                            </div>
+                            <p>dodaj plan</p>
+                        </div>
+                    </div>
+                    <div className="info_widget__container">
+                        <div className="info_widget__box">
+                            <div className="info_widget__content">
+                                <div className="info_widget__icon first">
+                                    <i class="fa fa-info"></i>
+                                </div>
+                                <p className="info_widget_paragraph__first">
+                                    Masz już 99 przepisów, nieźle!
+                                </p>
+                            </div>
+                            <div className="info_widget__close close_first">
+                                <i class="fa fa-times"></i>
+                            </div>
+                        </div>
+                        <div className="info_widget__box">
+                            <div className="info_widget__content">
+                                 <div className="info_widget__icon second">
+                                    <i class="fa fa-exclamation"></i>
+                                </div>
+                                <p className="info_widget_paragraph__second">
+                                    Pamiętaj, aby dodać plan!
+                                </p>
+                            </div>
+                            <div className="info_widget__close close_second ">
+                                <i class="fa fa-times"></i>
+                            </div>
+                        </div>
+                        <div className="info_widget__box">
+                            <div className="info_widget__content">
+                                <div className="info_widget__icon third">
+                                    <i class="fa fa-check"></i>
+                                </div>
+                                <p className="info_widget_paragraph__third">
+                                    Świetnie że jesteś! Udanego planowania i smacznego! :)
+                                </p>
+                            </div>
+                            <div className="info_widget__close close_third">
+                                <i class="fa fa-times"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="dashboard__plan">
+      <div className="dashboard_table__container">
+        <h4>Twój plan na nr_tygodnia tydzień:</h4>
+        <table className="dashboard__table">
+          <thead>
+            <tr className="dashboard_table__header">
+              <th>Poniedziałek</th>
+              <th>Wtorek</th>
+              <th>Środa</th>
+              <th>Czwartek</th>
+              <th>Piątek</th>
+              <th>Sobota</th>
+              <th>Niedziela</th>
+            </tr>
+            <tr>
+              <td>sniadanie</td>
+              <td>sniadanie</td>
+              <td>sniadanie</td>
+              <td>sniadanie</td>
+              <td>sniadanie</td>
+              <td>sniadanie</td>
+              <td>sniadanie</td>
+            </tr>
+            <tr>
+              <td>drugie sniadanie</td>
+              <td>drugie sniadanie</td>
+              <td>drugie sniadanie</td>
+              <td>drugie sniadanie</td>
+              <td>drugie sniadanie</td>
+              <td>drugie sniadanie</td>
+              <td>drugie sniadanie</td>
+            </tr>
+            <tr>
+              <td>zupa</td>
+              <td>zupa</td>
+              <td>zupa</td>
+              <td>zupa</td>
+              <td>zupa</td>
+              <td>zupa</td>
+              <td>zupa</td>
+            </tr>
+            <tr>
+              <td>drugie danie</td>
+              <td>drugie danie</td>
+              <td>drugie danie</td>
+              <td>drugie danie</td>
+              <td>drugie danie</td>
+              <td>drugie danie</td>
+              <td>drugie danie</td>
+            </tr>
+            <tr>
+              <td>kolacja</td>
+              <td>kolacja</td>
+              <td>kolacja</td>
+              <td>kolacja</td>
+              <td>kolacja</td>
+              <td>kolacja</td>
+              <td>kolacja</td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div className="dashboard__next_page">
+        <div className="dashboard_next_page__box">
+          <div>
+            <i class="fa fa-angle-double-left"></i>
+          </div>
+          <p>poprzedni</p>
+        </div>
+        <div className="dashboard_next_page__box">
+          <p>następny</p>
+          <div>
+            <i class="fa fa-angle-double-right"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+            </div>
         </div>
     )
 }
+
+export default Pulpit;
