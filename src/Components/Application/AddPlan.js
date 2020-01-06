a9ce9709ea4aca394d397d542aaec84d88d6bb98
Pietrzi <pietrzi@op.pl>
2020-01-06 21:38:12 +0100

    some more cleaning

--- src/Components/Application/AddPlan.js
+++ src/Components/Application/AddPlan.js
@@ -1,139 +1,6 @@
 import React from "react";
-// import Header from "./Header";
-// import Input from "./Input";
-// import Select from "./Select";
 
 class AddPlan extends React.Component {
-  // const [name, setName] = useState("");
-  // const [description, setDescription] = useState("");
-  // const [weekNumber, setWeekNumber] = useState("");
-  // const [monday, setMonday] = useState([]);
-  // const [tuesday, setTuesday] = useState([]);
-  // const [wednesday, setWednesday] = useState([]);
-  // const [thrusday, setThrusday] = useState([]);
-  // const [friday, setFriday] = useState([]);
-  // const [satruday, setSatruday] = useState([]);
-  // const [sunday, setSunday] = useState([]);
-  // const [plan, setPlan] = useState({
-  //   name: "",
-  //   description: "",
-  //   weekNumber: "",
-  //   monday: [],
-  //   tuesday: [],
-  //   wednesday: [],
-  //   thrusday: [],
-  //   friday: [],
-  //   satruday: [],
-  //   sunday: []
-  // });
-
-  // //validation
-  // let errors = {
-  //   name: "",
-  //   description: "",
-  //   weekNumber: "",
-  //   monday: "",
-  //   tuesday: "",
-  //   wednesday: "",
-  //   thrusday: "",
-  //   friday: "",
-  //   satruday: "",
-  //   sunday: ""
-  // };
-
-  // let isValid = true;
-
-  // const validate = () => {
-  //   if (!name) {
-  //     isValid = false;
-  //     errors.name = "Podaj nazwę planu!";
-  //   } else {
-  //     plan.name = name;
-  //     errors.name = "";
-  //   }
-
-  //   if (!description) {
-  //     errors.description = "Podaj opis planu!";
-  //   } else {
-  //     plan.description = description;
-  //     errors.description = "";
-  //   }
-
-  //   if (!weekNumber) {
-  //     isValid = false;
-  //     errors.weekNumber = "Podaj numer tygodnia!";
-  //   } else {
-  //     plan.weekNumber = weekNumber;
-  //     errors.weekNumber = "";
-  //   }
-
-  //   if (!monday) {
-  //     errors.monday = "pusta";
-  //   } else {
-  //     plan.monday = monday;
-  //     errors.monday = "";
-  //   }
-  //   if (!tuesday) {
-  //     errors.tuesday = "pusta";
-  //   } else {
-  //     plan.tuesday = tuesday;
-  //     errors.tuesday = "";
-  //   }
-  //   if (!wednesday) {
-  //     errors.wednesday = "pusta";
-  //   } else {
-  //     plan.wednesday = wednesday;
-  //     errors.wednesday = "";
-  //   }
-  //   if (!thrusday) {
-  //     errors.monday = "pusta";
-  //   } else {
-  //     plan.thrusday = thrusday;
-  //     errors.thrusday = "";
-  //   }
-  //   if (!friday) {
-  //     errors.friday = "pusta";
-  //   } else {
-  //     plan.friday = friday;
-  //     errors.friday = "";
-  //   }
-  //   if (!satruday) {
-  //     errors.satruday = "pusta";
-  //   } else {
-  //     plan.satruday = satruday;
-  //     errors.satruday = "";
-  //   }
-  //   if (!sunday) {
-  //     errors.sunday = "pusta";
-  //   } else {
-  //     plan.sunday = sunday;
-  //     errors.sunday = "";
-  //   }
-
-  //   setPlan({ ...plan });
-
-  //   return isValid;
-  // };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   if (validate()) {
-  //     addPlan(plan);
-  //     setName("");
-  //     setDescription("");
-  //     setWeekNumber("");
-  //     onClose();
-  //     alert("Dziękujemy za dodanie planu!");
-  //   } else {
-  //     // console.log("Formularz błędnie wypełniony");
-  //     console.log(errors);
-  //     return errors;
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchRecipes();
-  // }, [fetchRecipes]);
   render() {
     return (
       <div className='addPlan__container modal '>
@@ -141,9 +8,6 @@ class AddPlan extends React.Component {
             <i className='fa fa-times close-btn' aria-hidden='true'></i>
             <div className='addPlan_header__box'>
               <h2>NOWY PLAN</h2>
-              {/* {name.length <= 0 || !weekNumber ? (
-                <ErrorMessage>Uzupełnij zaznaczone pola!</ErrorMessage>
-                ) : null} */}
               <button type='submit'>Zapisz i zamknij</button>
             </div>
             <div className='addPlan__input'>
@@ -159,9 +23,6 @@ class AddPlan extends React.Component {
                     // className={name.length <= 0 ? "error" : null}
                   />
                 </div>
-
-                {/* {name.length >= 50 && <ErrorMessage>Nazwa jest za długa!</ErrorMessage>} */}
-
                   <div className='addPlan_input__el'>
                     <h3>Opis planu</h3>
                     <textarea
@@ -172,11 +33,6 @@ class AddPlan extends React.Component {
                       // onChange={addDescription}
                       placeholder='Opis planu'></textarea>
                   </div>
-
-                    {/* {description.length >= 360 && (
-                    <ErrorMessage>Opis jest za długi!</ErrorMessage>
-                    )} */}
-
                   <div className='addPlan_input__week'>
                     <h3>Numer tygodnia</h3>
                     <input
