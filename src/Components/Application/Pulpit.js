72c0f6ed0366cc6fbdd51494d618eb55142d908c
Pietrzi <pietrzi@op.pl>
2020-01-07 11:14:31 +0100

    improved a bit styling

--- src/Components/Application/Pulpit.js
+++ src/Components/Application/Pulpit.js
@@ -7,7 +7,7 @@ class Pulpit extends React.Component {
     const { addPlan, addRecipe } = this.props
 
     return (
-        <div className="pulpit">
+        
             <div className="dashboard">
                 <div className="dashboard__widgets">
                     <div className="add_widget__container">
@@ -67,84 +67,85 @@ class Pulpit extends React.Component {
                     </div>
                 </div>
                 <div className="dashboard__plan">
-      <div className="dashboard_table__container">
-        <h4>Twój plan na nr_tygodnia tydzień:</h4>
-        <table className="dashboard__table">
-          <thead>
-            <tr className="dashboard_table__header">
-              <th>Poniedziałek</th>
-              <th>Wtorek</th>
-              <th>Środa</th>
-              <th>Czwartek</th>
-              <th>Piątek</th>
-              <th>Sobota</th>
-              <th>Niedziela</th>
-            </tr>
-            <tr>
-              <td>sniadanie</td>
-              <td>sniadanie</td>
-              <td>sniadanie</td>
-              <td>sniadanie</td>
-              <td>sniadanie</td>
-              <td>sniadanie</td>
-              <td>sniadanie</td>
-            </tr>
-            <tr>
-              <td>drugie sniadanie</td>
-              <td>drugie sniadanie</td>
-              <td>drugie sniadanie</td>
-              <td>drugie sniadanie</td>
-              <td>drugie sniadanie</td>
-              <td>drugie sniadanie</td>
-              <td>drugie sniadanie</td>
-            </tr>
-            <tr>
-              <td>zupa</td>
-              <td>zupa</td>
-              <td>zupa</td>
-              <td>zupa</td>
-              <td>zupa</td>
-              <td>zupa</td>
-              <td>zupa</td>
-            </tr>
-            <tr>
-              <td>drugie danie</td>
-              <td>drugie danie</td>
-              <td>drugie danie</td>
-              <td>drugie danie</td>
-              <td>drugie danie</td>
-              <td>drugie danie</td>
-              <td>drugie danie</td>
-            </tr>
-            <tr>
-              <td>kolacja</td>
-              <td>kolacja</td>
-              <td>kolacja</td>
-              <td>kolacja</td>
-              <td>kolacja</td>
-              <td>kolacja</td>
-              <td>kolacja</td>
-            </tr>
-          </thead>
-        </table>
-      </div>
-      <div className="dashboard__next_page">
-        <div className="dashboard_next_page__box">
-          <div>
-            <i class="fa fa-angle-double-left"></i>
-          </div>
-          <p>poprzedni</p>
-        </div>
-        <div className="dashboard_next_page__box">
-          <p>następny</p>
-          <div>
-            <i class="fa fa-angle-double-right"></i>
-          </div>
-        </div>
-      </div>
-    </div>
+                  <div className="dashboard_table__container">
+                    <h4>Twój plan na nr_tygodnia tydzień:</h4>
+                    <table className="dashboard__table">
+                      <thead>
+                        <tr className="dashboard_table__header">
+                          <th>Poniedziałek</th>
+                          <th>Wtorek</th>
+                          <th>Środa</th>
+                          <th>Czwartek</th>
+                          <th>Piątek</th>
+                          <th>Sobota</th>
+                          <th>Niedziela</th>
+                        </tr>
+                        <tr>
+                          <td>sniadanie</td>
+                          <td>sniadanie</td>
+                          <td>sniadanie</td>
+                          <td>sniadanie</td>
+                          <td>sniadanie</td>
+                          <td>sniadanie</td>
+                          <td>sniadanie</td>
+                        </tr>
+                        <tr>
+                          <td>drugie sniadanie</td>
+                          <td>drugie sniadanie</td>
+                          <td>drugie sniadanie</td>
+                          <td>drugie sniadanie</td>
+                          <td>drugie sniadanie</td>
+                          <td>drugie sniadanie</td>
+                          <td>drugie sniadanie</td>
+                        </tr>
+                        <tr>
+                          <td>zupa</td>
+                          <td>zupa</td>
+                          <td>zupa</td>
+                          <td>zupa</td>
+                          <td>zupa</td>
+                          <td>zupa</td>
+                          <td>zupa</td>
+                        </tr>
+                        <tr>
+                          <td>drugie danie</td>
+                          <td>drugie danie</td>
+                          <td>drugie danie</td>
+                          <td>drugie danie</td>
+                          <td>drugie danie</td>
+                          <td>drugie danie</td>
+                          <td>drugie danie</td>
+                        </tr>
+                        <tr>
+                          <td>kolacja</td>
+                          <td>kolacja</td>
+                          <td>kolacja</td>
+                          <td>kolacja</td>
+                          <td>kolacja</td>
+                          <td>kolacja</td>
+                          <td>kolacja</td>
+                        </tr>
+                      </thead>
+                    </table>
+                  </div>
+                  <div className="dashboard__next_page">
+                    <div className="dashboard_next_page__box">
+                      <div>
+                        <i class="fa fa-angle-double-left"></i>
+                      </div>
+                      <p>poprzedni</p>
+                    </div>
+                    <div className="dashboard_next_page__box">
+                      <p>następny</p>
+                      <div>
+                        <i class="fa fa-angle-double-right"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
             </div>
-        </div>
+          
+       
     )
   }
   
