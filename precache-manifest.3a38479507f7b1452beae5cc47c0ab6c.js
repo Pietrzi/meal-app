0d0abb98bd12d6d8ca1e955f971870e44e56b38b
Pietrzi <pietrzi@op.pl>
2020-01-15 13:31:45 +0100

    Updates

---
+++ precache-manifest.3a38479507f7b1452beae5cc47c0ab6c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f62392d7352b80ac935fdb83639680",
+    "url": "/meal-planner/index.html"
+  },
+  {
+    "revision": "31813ba5ae0171ffa2f8",
+    "url": "/meal-planner/static/css/2.eb0dd858.chunk.css"
+  },
+  {
+    "revision": "312d7e3d1626ee366b50",
+    "url": "/meal-planner/static/css/main.31fddcf5.chunk.css"
+  },
+  {
+    "revision": "31813ba5ae0171ffa2f8",
+    "url": "/meal-planner/static/js/2.25087773.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/meal-planner/static/js/2.25087773.chunk.js.LICENSE"
+  },
+  {
+    "revision": "312d7e3d1626ee366b50",
+    "url": "/meal-planner/static/js/main.0746c259.chunk.js"
+  },
+  {
+    "revision": "d9e18cc6c730bbba29ee",
+    "url": "/meal-planner/static/js/runtime-main.3b9746d4.js"
+  },
+  {
+    "revision": "1e704525e44c1372aec35bff52b44fae",
+    "url": "/meal-planner/static/media/author.1e704525.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/meal-planner/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/meal-planner/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/meal-planner/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/meal-planner/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/meal-planner/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6be1d45aca1180fbaf4a8b6463bdd19b",
+    "url": "/meal-planner/static/media/meal1.6be1d45a.jpg"
+  },
+  {
+    "revision": "be394d897c5c8e671c771f74d75b8ef9",
+    "url": "/meal-planner/static/media/meal2.be394d89.jpg"
+  },
+  {
+    "revision": "27ac60b5c867a06fc4e3e739f8287363",
+    "url": "/meal-planner/static/media/meal3.27ac60b5.jpg"
+  }
+]);
